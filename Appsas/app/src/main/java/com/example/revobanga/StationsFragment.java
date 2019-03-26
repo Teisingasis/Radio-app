577c209cb4688469cb1461ff3b4b76437e46c4f0
RokasJan <48027184+RokasJan@users.noreply.github.com>
2019-03-27 00:55:10 +0200

    Current stream info
    Added current stream information but for more results requires a database with songs list

--- Appsas/app/src/main/java/com/example/revobanga/StationsFragment.java
+++ Appsas/app/src/main/java/com/example/revobanga/StationsFragment.java
@@ -10,51 +10,55 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.io.IOException;
 
-public class StationsFragment extends Fragment{
+public class StationsFragment extends Fragment {
 
     public MediaPlayer mediaPlayer;
     public String url;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view= inflater.inflate(R.layout.fragment_radio_stations, container, false);
+        View view = inflater.inflate(R.layout.fragment_radio_stations, container, false);
         mediaPlayer = new MediaPlayer();
         mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-    firstStation(view);
-    secondStation(view);
-    return  view;
+        firstStation(view);
+        secondStation(view);
+        return view;
     }
-    private void firstStation(View view){
+
+    private void firstStation(View view) {
+        final TextView currentsong = view.findViewById(R.id.current);
         Button pirmStotis = view.findViewById(R.id.button3);
-        url ="http://www.kozco.com/tech/32.mp3";
+        url = "http://www.kozco.com/tech/32.mp3";
         pirmStotis.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                getFragmentManager().beginTransaction().replace(R.id.fragment_container,new Station1Fragment()).commit();
+                getFragmentManager().beginTransaction().replace(R.id.fragment_container, new Station1Fragment()).commit();
                 try {
                     mediaPlayer.setDataSource(url);
                     mediaPlayer.prepare();
                     mediaPlayer.start();
+                    currentsong.setText("First Station");
                 } catch (IOException e) {
                     e.printStackTrace();
+                    currentsong.setText("Current Station");
                 }
-
             }
         });
     }
-    private void secondStation(View view){
+
+    private void secondStation(View view) {
         Button antrStotis = view.findViewById(R.id.button4);
         antrStotis.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getFragmentManager().beginTransaction().replace(R.id.fragment_container,new Station2Fragment()).commit();
+                getFragmentManager().beginTransaction().replace(R.id.fragment_container, new Station2Fragment()).commit();
             }
         });
     }
-
-
 }
