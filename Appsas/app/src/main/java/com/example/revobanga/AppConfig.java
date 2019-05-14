d246b33911527b0f24d0d89dbaab84cd685e0332
RokasJan <rokas.janoniss@gmail.com>
2019-05-14 23:47:10 +0300

    Database REST API and JSON
    Database REST API and JSON

---
+++ Appsas/app/src/main/java/com/example/revobanga/AppConfig.java
@@ -0,0 +1,9 @@
+package com.example.revobanga;
+
+public class AppConfig {
+
+    public static String URL = "https://alexbordei.com/";
+    public static String ENDPOINT_LOGIN = "wp-json/user/login";
+    public static String ENDPOINT_REGISTER = "wp-json/user/register";
+
+}
